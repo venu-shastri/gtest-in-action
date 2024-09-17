97f3e0322d18ac211625c96b5dfc2cb0538c484c
Venugopal K <venu.shastri@gmail.com>
2024-09-17 16:40:44 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,9 +1,35 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
+class StringCalculatorFixture:public testing::Test{
+protected:
+    string input;
+    int exepctedValue;
+    int actualValue;
+};
+
+TEST_F(StringCalculatorFixture,add_emptyInputString_ZeroIsExpected){
+  //Arrange
+   input="";
+  expectedValue=0;
+  //Act
+  actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
+
 TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
   //Arrange
   string input="";
+  int expectedValue=0;
+  //Act
+  int actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
+TEST(StringCalculatorTestSuite,add_InputZero_ZeroIsExpected){
+  //Arrange
+  string input="0";
   int expectedValue=0;
   //Act
   int actualValue=Add(input);

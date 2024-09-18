33f9189ec0c371d7a5a11a7dec1d7da37c44d43c
Venugopal K <venu.shastri@gmail.com>
2024-09-18 16:04:20 +0530

    Create TestChallenge.cpp

---
+++ TestChallenge.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+ 
+int alertFailureCount = 0;
+ 
+int networkAlerter(float celcius) {
+   //Communicate with Network, Require Internet Connectivity and credentials
+  //If Communication is successfull , returns 200 otherwise 500
+    return 500;
+}
+ 
+void alertInCelcius(float farenheit) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlertStub(celcius);
+    if (returnCode != 200) {
+        alertFailureCount += 0;
+    }
+}
+ 

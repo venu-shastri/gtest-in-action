70c65ebf960b5c066b0033beda4bd11b083170b8
Venugopal K <venu.shastri@gmail.com>
2024-09-18 17:40:37 +0530

    Update TestChallengeTests.cpp

--- TestChallengeTests.cpp
+++ TestChallengeTests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "TestChallenge.h"
-
+#include <gmock/gmock.h>
+using testing::FloatEq;//matcher used to asset on method arguments
 //Stub - Fake Dependency
 class NetworkAlerterStub:public INetworkAlerter{
 public:
@@ -9,8 +10,24 @@ public:
       }
 
 };
+
+//Mock Implementation using Gmock
+class NetworkAlerterMock:public INetworkAlerter{
+public:
+             MOCK_METHOD(int ,alert,(float celcius));
+};
+
 TEST(NetworkAlerterTestSuite,StateBasedTest){
   NetworkAlerterStub stub;
     int failureCount=  alertInCelciusFacade(400,&stub);
     ASSERT_EQ(failureCount,1);
+}
+TEST(NetworkAlerterTestSuite,InteractionTest){
+    NetworkAlerterMock mock;
+      // EXPECT_CALL (mockObject,method(Matcher).Times(Frequency).WillOnce/Repeat(Action)
+      
+  EXPECT_CALL(mock,alert(FloatEq(204.44))).Times(1).WillOnce(Return(500));
+    int failureCount=  alertInCelciusFacade(400,&mock);
+    ASSERT_EQ(failureCount,1);
+    
 }

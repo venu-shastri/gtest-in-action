8bb444a1f763f9e024879c0597172fc314d081e9
Venugopal K <venu.shastri@gmail.com>
2024-09-18 14:27:01 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -43,13 +43,14 @@ vector<int> get_numbers(const string &input) {
 	return numbers;
 }
 
-void  Add(const string &input) {
+
+void  Add(const string &input,void(*displayPtr)(int)) {
 	if (input.empty())
-		cout<< 0;
+		(*displayPtr)(0);
 
 	vector<int> numbers = get_numbers(input);
 
 	check_for_negatives(numbers);
 
-	cout<<accumulate(numbers.begin(), numbers.end(), 0);
+	(*displayPtr)(accumulate(numbers.begin(), numbers.end(), 0));
 }

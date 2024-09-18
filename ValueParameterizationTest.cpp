7bb58f7fc6bb0023715df937dec68856350d5703
Venugopal K <venu.shastri@gmail.com>
2024-09-18 10:46:09 +0530

    Update ValueParameterizationTest.cpp

--- ValueParameterizationTest.cpp
+++ ValueParameterizationTest.cpp
@@ -1,1 +1,32 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+#include <tuple>
+
+class StringCalculatorFixture:public testing::Test{
+protected:
+    string input;
+    int expectedValue;
+    int actualValue;
+};
+class StringCalculatorParameterFixture:public StringCalculatorFixture, public testing::WithParamInterface<tuple<string,int>>{
+
+};
+//Parameter Values
+INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFixture,testing::Values(
+  make_tuple("", 0),
+  make_tuple("0", 0),
+  make_tuple("1", 1),
+  make_tuple("1,2", 3),
+  make_tuple("1,2,3", 6)
+  
+));
+
+TEST_P(StringCalculatorParameterFixture,ParameterizedTest){
+      input= std::get<0>(GetParam());
+      expectedValue= std::get<1>(GetParam());
+      actualValue=Add(input);
+      ASSERT_EQ(actualValue,expectedValue);
+}
+
+
 
